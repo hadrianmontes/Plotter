0dbb155e4a57318d2e42e3152ea31b69220f4a4b
Hadrian Montes <hadrianmontes@gmail.com>
2016-06-08 00:32:18 +0200

    Added function to stick the axis

--- template.py
+++ template.py
@@ -2,6 +2,48 @@ import matplotlib
 matplotlib.use("Qt4Agg")
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.ticker import MaxNLocator
+
+def stick_axes(fig):
+    """
+    Stick the axes of a figure 'fig'. The subplots distribution is respected.
+    """
+    # Se leen los ejes de la figura
+    l_axes = fig.axes
+    # El número de columnas y filas
+    nrows, ncols, _, _ = l_axes[0].get_subplotspec().get_geometry()
+    # Se inicia una lista de ceros con la forma del grid
+    axes = [[0 for j in range(ncols)] for i in range(nrows)]
+    # Se sitúa cada eje en su sitio de la grid
+    for ax in l_axes:
+        _, _, ind, _ = ax.get_subplotspec().get_geometry()
+        row, col = ind//ncols, ind%ncols
+        axes[row][col] = ax
+    # Se quita el interespaciado
+    plt.subplots_adjust(hspace=0.001)
+    plt.subplots_adjust(wspace=0.001)
+    # Se eliminan las etiquetas que queden en los intersticios
+    # Primero en x
+    print(axes)
+    if nrows > 1:
+        for i in range(nrows-1):
+            for j in range(ncols):
+                axes[i][j].set_xticklabels([])
+        # Se eliminan las etiquetas que solapan
+        nbins = len(axes[-1][-1].get_xticklabels())
+        for j in range(nrows):
+            for i in range(ncols-1):
+                axes[j][i].xaxis.set_major_locator(MaxNLocator(nbins=nbins, prune='upper'))
+    # Ahora en y
+    if ncols > 1:
+        for j in range(ncols-1):
+            for i in range(nrows):
+                axes[i][j+1].set_yticklabels([])
+        # Se eliminan las etiquetas que solapan
+        nbins = len(axes[-1][0].get_xticklabels())
+        for i in range(nrows-1):
+            for j in range(ncols):
+                axes[i+1][j].yaxis.set_major_locator(MaxNLocator(nbins=nbins, prune='upper'))
 
 def read_data(filename,xcolumn,ycolumn):
     f=open(filename,"r")
